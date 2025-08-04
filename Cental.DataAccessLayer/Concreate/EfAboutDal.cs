fd8032fb13c4494fd89dfe5df5e5f48566cd0655
FarukPie <farukatak.official@gmail.com>
2025-08-04 20:07:52 +0300

    I have completed new abstract sections

---
+++ Cental.DataAccessLayer/Concreate/EfAboutDal.cs
@@ -0,0 +1,19 @@
+﻿using Cental.DataAccessLayer.Abstract;
+using Cental.DataAccessLayer.Context;
+using Cental.DataAccessLayer.Repostories;
+using Cental.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.DataAccessLayer.Concreate
+{
+    public class EfAboutDal : GenericRepostories<About>
+    {
+        public EfAboutDal(CentalContext context) : base(context)
+        {
+        }
+    }
+}
