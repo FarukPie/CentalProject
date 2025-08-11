5c986016f15276ceee0eb7efe5060ab99182f9aa
FarukPie <farukatak.official@gmail.com>
2025-08-11 23:28:28 +0300

    I have seperated about and banner section

--- Cental.BusinessLayer/Concreate/BannerManager.cs
+++ Cental.BusinessLayer/Concreate/BannerManager.cs
@@ -9,10 +9,38 @@ using System.Threading.Tasks;
 
 namespace Cental.BusinessLayer.Concreate
 {
-    public class BannerManager : GenericManager<Banner>, IBannerService
+    public class BannerManager : IBannerService
     {
-        public BannerManager(IGenericDal<Banner> genericDal) : base(genericDal)
+        private readonly IBannerDal _bannerdal;
+
+        public BannerManager(IBannerDal bannerdal)
         {
+            _bannerdal = bannerdal;
+        }
+
+        public void TCreate(Banner entity)
+        {
+           _bannerdal.Create(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _bannerdal.Delete(id);  
+        }
+
+        public List<Banner> TGetAll()
+        {
+           return _bannerdal.GetAll();
+        }
+
+        public Banner TGetById(int id)
+        {
+            return _bannerdal.GetById(id);
+        }
+
+        public void TUpdate(Banner entity)
+        {
+            _bannerdal.Update(entity);
         }
     }
 }
--- Cental.BusinessLayer/Concreate/BrandManager.cs
+++ Cental.BusinessLayer/Concreate/BrandManager.cs
@@ -9,10 +9,38 @@ using System.Threading.Tasks;
 
 namespace Cental.BusinessLayer.Concreate
 {
-    public class BrandManager : GenericManager<Brand>, IBrandService
+    public class BrandManager : IBrandService
     {
-        public BrandManager(IGenericDal<Brand> genericDal) : base(genericDal)
+        private readonly IBrandDal _branddal;
+
+        public BrandManager(IBrandDal branddal)
         {
+            _branddal = branddal;
+        }
+
+        public void TCreate(Brand entity)
+        {
+            _branddal.Create(entity);   
+        }
+
+        public void TDelete(int id)
+        {
+           _branddal.Delete(id);
+        }
+
+        public List<Brand> TGetAll()
+        {
+           return _branddal.GetAll();
+        }
+
+        public Brand TGetById(int id)
+        {
+            return _branddal.GetById(id);   
+        }
+
+        public void TUpdate(Brand entity)
+        {
+           _branddal.Update(entity);
         }
     }
 }
