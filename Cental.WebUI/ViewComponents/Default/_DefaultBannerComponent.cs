e5b15105ad7f1dc974cf36a226737144a490137f
FarukPie <farukatak.official@gmail.com>
2025-08-13 15:51:03 +0300

    I have separated view component UILayout and AdminLayout

---
+++ Cental.WebUI/ViewComponents/Default/_DefaultBannerComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultBannerComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Cental.WebUI/ViewComponents/Default/_DefaultHeaderComponent.cs
@@ -0,0 +1,15 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DTOLayer.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultHeaderComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+           
+            return View();
+        }
+    }
+}
