6a71de3cb931d88adc03f7621ddf20aef96719e7
FarukPie <farukatak.official@gmail.com>
2025-08-14 02:24:03 +0300

    I did solve brand section bugs and I did adjustment

--- Cental.WebUI/Controllers/AdminBrandController.cs
+++ Cental.WebUI/Controllers/AdminBrandController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Cental.BusinessLayer.Abstract;
+using Cental.DTOLayer.BannerDtos;
 using Cental.DTOLayer.BrandDtos;
 using Cental.EntityLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -32,8 +33,34 @@ namespace Cental.WebUI.Controllers
         {
             var brand = _mapper.Map<Brand>(model);
             _brandservice.TCreate(brand);
-            return View(brand);
+            return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteBrand(int id) 
+        {
+            _brandservice.TDelete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateBrand(int id)
+        {
+            var entity = _brandservice.TGetById(id);
+            var dto = _mapper.Map<ResultBrandDto>(entity); // Entity → DTO
+            return View(dto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateBrand(UpdateBrandDto model)
+        {
+         
+
+            var banner = _mapper.Map<Brand>(model); // DTO → Entity
+            _brandservice.TUpdate(banner);
+
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
