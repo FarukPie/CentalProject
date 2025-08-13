350e520f8a17661e9ffbc33e0bd1245dbb6ade1f
FarukPie <farukatak.official@gmail.com>
2025-08-14 01:41:30 +0300

    adjustment

---
+++ Cental.WebUI/Mappings/FeatureMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Cental.DTOLayer.FeatureDtos;
+using Cental.EntityLayer.Entities;
+
+namespace Cental.WebUI.Mappings
+{
+    public class FeatureMapping: Profile
+    {
+        public FeatureMapping() 
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+        
+        
+        }
+    }
+}
