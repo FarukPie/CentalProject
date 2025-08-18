ed25bb2dccbd51a353d2980a230f0b61d1d00187
FarukPie <Farukatak.official@gmail.com>
2025-08-18 14:11:45 +0300

    admin panel layout adjustments and I have added enums

--- Cental.WebUI/Controllers/AdminCarController.cs
+++ Cental.WebUI/Controllers/AdminCarController.cs
@@ -2,8 +2,11 @@
 using Cental.BusinessLayer.Abstract;
 using Cental.DTOLayer.BrandDtos;
 using Cental.DTOLayer.CarDtos;
+using Cental.DTOLayer.Enums;
 using Cental.EntityLayer.Entities;
+using Cental.WebUI.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Cental.WebUI.Controllers
 {
@@ -11,15 +14,18 @@ namespace Cental.WebUI.Controllers
     {
         private readonly ICarService _carService;
         private readonly IMapper _mapper;
+        private readonly IBrandService _brandService;
 
-        public AdminCarController(ICarService carService, IMapper mapper)
+        public AdminCarController(ICarService carService, IMapper mapper, IBrandService brandService)
         {
             _carService = carService;
             _mapper = mapper;
+            _brandService = brandService;
         }
 
         public IActionResult Index()
         {
+           
             var value = _carService.TGetCarsWithBrands();
             var car=_mapper.Map<List<ResultCarDto>>(value);
             return View(car);
@@ -27,11 +33,27 @@ namespace Cental.WebUI.Controllers
         [HttpGet]
         public IActionResult CreateCar() 
         {
-        return View();
+            ViewBag.brands = (from x in _brandService.TGetAll()
+                              select new SelectListItem
+                              {
+                                  Text = x.BrandName,
+                                  Value = x.BrandID.ToString()
+                              }).ToList();
+            ViewBag.gasType = GetEnumValues.GetEnums<GasType>();
+            ViewBag.gearType = GetEnumValues.GetEnums<GearType>();
+            return View();
         }
         [HttpPost]  
         public IActionResult CreateCar(CreateCarDto dto)
         {
+            ViewBag.gasType = GetEnumValues.GetEnums<GasType>();
+            ViewBag.gearType = GetEnumValues.GetEnums<GearType>();
+            ViewBag.brands=(from x in _brandService.TGetAll()
+                            select new SelectListItem
+                            {
+                                Text=x.BrandName,
+                                Value=x.BrandID.ToString()
+                            }).ToList();
             var car = _mapper.Map<Car>(dto);
             _carService.TCreate(car);
             return RedirectToAction("Index");
