21d3eb23516a4736d7858a099ab4efb11aedb49b
FarukPie <Farukatak.official@gmail.com>
2025-08-24 17:17:45 +0300

    sign up and register sections are adjusted

---
+++ Cental.BusinessLayer/Validators/CustomErrorDescriber.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.BusinessLayer.Validators
+{
+    public class CustomErrorDescriber: IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Description = "password must contain at least 6 characters"
+            };
+        }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Description = "Password must contain at least one number"
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Description = "password must contain at least one uppercase letter"
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Description = "password must contain special characters"
+            };
+        }
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Description = $"{userName} This username is already taken"
+            };
+            
+        }
+    }
+}
